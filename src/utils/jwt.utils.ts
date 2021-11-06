4011851841f998b9a5dc62348bce755c9bde489e
Kelvin Bush <kelybush@gmail.com>
2021-11-06 20:40:20 +0300

    verify jwt
    Signed-off-by: Kelvin Bush <kelybush@gmail.com>

--- src/utils/jwt.utils.ts
+++ src/utils/jwt.utils.ts
@@ -1,6 +1,29 @@
 import jwt from "jsonwebtoken";
 import config from "config";
 
-function signJwt() {}
+const privateKey = config.get<string>("privateKey");
+const publicKey = config.get<string>("publicKey");
 
-function verifyJwt() {}
+export function signJwt(object: Object, options?: jwt.SignOptions | undefined) {
+  return jwt.sign(object, privateKey, {
+    ...(options && options),
+    algorithm: "RS256",
+  });
+}
+
+function verifyJwt(token: string) {
+  try {
+    const decoded = jwt.verify(token, publicKey);
+    return {
+      valid: true,
+      expired: false,
+      decoded,
+    };
+  } catch (e: any) {
+    return {
+      valid: false,
+      expired: e.message === "jwt expired",
+      decoded: null,
+    };
+  }
+}
