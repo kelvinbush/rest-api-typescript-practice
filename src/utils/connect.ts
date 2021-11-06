692dc44f8ea4f3d72c0ca3ef15e54f24a9e88142
Kelvin Bush <kelybush@gmail.com>
2021-11-06 13:29:07 +0300

    configuring logger
    Signed-off-by: Kelvin Bush <kelybush@gmail.com>

--- src/utils/connect.ts
+++ src/utils/connect.ts
@@ -1,12 +1,14 @@
 import mongoose from "mongoose";
 import config from "config";
+import logger from "./logger";
 
 async function connect() {
   const dbUri = config.get<string>("dbUri");
   try {
     await mongoose.connect(dbUri);
+    logger.info("db connected");
   } catch (e) {
-    console.error(e);
+    logger.error(e);
     process.exit(1);
   }
 }
