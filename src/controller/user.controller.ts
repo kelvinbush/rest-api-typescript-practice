bff30e4c83dba858502a321f4c688dc49bbb1b49
Kelvin Bush <kelybush@gmail.com>
2021-11-06 14:35:57 +0300

    configure validation with zod
    Signed-off-by: Kelvin Bush <kelybush@gmail.com>

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -1,8 +1,10 @@
 import { Request, Response } from "express";
 import logger from "../utils/logger";
+import { createUser } from "../service/user.service";
 
-export function createUserHandler(req: Request, res: Response) {
+export async function createUserHandler(req: Request, res: Response) {
   try {
+    return await createUser(req.body);
   } catch (e: any) {
     logger.error(e);
     return res.status(409).send(e.message);
