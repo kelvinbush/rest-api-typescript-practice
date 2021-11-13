227ca7c20e9daf50aeaed9878f9160a5eea8c3e0
Kelvin Bush <kelybush@gmail.com>
2021-11-13 14:47:08 +0300

    resolving the form with zod
    Signed-off-by: Kelvin Bush <kelybush@gmail.com>

--- ui/pages/auth/register.tsx
+++ ui/pages/auth/register.tsx
@@ -1,14 +1,43 @@
 import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { object, string } from "zod";
+
+const createUserSchema = object({
+  name: string({
+    required_error: "Name is required",
+  }),
+  password: string({
+    required_error: "Password is required",
+  }).min(6, "Password too short - should be 6 chars minimum"),
+  passwordConfirmation: string({
+    required_error: "passwordConfirmation is required",
+  }),
+  email: string({
+    required_error: "Email is required",
+  }).email("Not a valid email"),
+}).refine((data) => data.password === data.passwordConfirmation, {
+  message: "Passwords do not match",
+  path: ["passwordConfirmation"],
+});
 
 function RegisterPage() {
   const {
     register,
     formState: { errors },
-  } = useForm();
+    handleSubmit,
+  } = useForm({
+    resolver: zodResolver(createUserSchema),
+  });
+
+  function onSubmit(values: any) {
+    console.log({ values });
+  }
+
+  console.log({ errors });
 
   return (
     <>
-      <form>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <div className={"form-element"}>
           <label htmlFor={"email"}>Email</label>
           <input
@@ -19,6 +48,7 @@ function RegisterPage() {
           />
           <p>{errors.email?.message}</p>
         </div>
+        <button type={"submit"}>Submit</button>
       </form>
     </>
   );
