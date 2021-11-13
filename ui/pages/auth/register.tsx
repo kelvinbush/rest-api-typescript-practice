208499cc83b7888291b2ded6650e01cb19d99289
Kelvin Bush <kelybush@gmail.com>
2021-11-13 14:56:44 +0300

    fix validation on strings
    Signed-off-by: Kelvin Bush <kelybush@gmail.com>

--- ui/pages/auth/register.tsx
+++ ui/pages/auth/register.tsx
@@ -3,18 +3,24 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { object, string } from "zod";
 
 const createUserSchema = object({
-  name: string({
-    required_error: "Name is required",
+  name: string().nonempty({
+    message: "Name is required",
   }),
-  password: string({
-    required_error: "Password is required",
-  }).min(6, "Password too short - should be 6 chars minimum"),
-  passwordConfirmation: string({
-    required_error: "passwordConfirmation is required",
+  password: string()
+    .min(6, "Password too short - should be 6 chars minimum")
+    .nonempty({
+      message: "Password is required",
+    }),
+  passwordConfirmation: string().nonempty({
+    message: "passwordConfirmation is required",
   }),
   email: string({
     required_error: "Email is required",
-  }).email("Not a valid email"),
+  })
+    .email("Not a valid email")
+    .nonempty({
+      message: "Password is required",
+    }),
 }).refine((data) => data.password === data.passwordConfirmation, {
   message: "Passwords do not match",
   path: ["passwordConfirmation"],
@@ -47,6 +53,36 @@ function RegisterPage() {
             {...register("email")}
           />
           <p>{errors.email?.message}</p>
+        </div>
+        <div className={"form-element"}>
+          <label htmlFor={"name"}>Name</label>
+          <input
+            id={"name"}
+            type="text"
+            placeholder={"Jane Doe"}
+            {...register("name")}
+          />
+          <p>{errors.name?.message}</p>
+        </div>
+        <div className={"form-element"}>
+          <label htmlFor={"password"}>Password</label>
+          <input
+            id={"password"}
+            type="password"
+            placeholder={"*********"}
+            {...register("password")}
+          />
+          <p>{errors.password?.message}</p>
+        </div>
+        <div className={"form-element"}>
+          <label htmlFor={"passwordConfirmation"}>Confirm Password</label>
+          <input
+            id={"passwordConfirmation"}
+            type="password"
+            placeholder={"*************"}
+            {...register("passwordConfirmation")}
+          />
+          <p>{errors.passwordConfirmation?.message}</p>
         </div>
         <button type={"submit"}>Submit</button>
       </form>
